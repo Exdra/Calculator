a612a27a5c4f11514a82e6c31ebbdbc746113f80
Exdra <108461934+Exdra@users.noreply.github.com>
2022-07-16 05:32:42 -0400

    Add files via upload

---
+++ c# calc/calc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace csharp
+{
+    class calc
+    {
+        static void Main(string[] args)
+        {
+            float res;
+            
+            Console.WriteLine("pls enter your first number");
+            string num1 = Console.ReadLine();
+            Console.WriteLine("pls enter your second number");
+            string num2 = Console.ReadLine();
+            Console.WriteLine("pls enter your operation sign");
+            string sign = Console.ReadLine();
+
+            if (sign == "+")
+            {
+               res = Convert.ToInt32(num1) + Convert.ToInt32(num2);
+               Console.WriteLine("your answer is "+res);
+            }
+            else if (sign == "-")
+            {
+                res = Convert.ToInt32(num1) - Convert.ToInt32(num2);
+                Console.WriteLine("your answer is "+res);
+            }
+            else if (sign == "*")
+            {
+                res = Convert.ToInt32(num1) * Convert.ToInt32(num2);
+                Console.WriteLine("your answer is "+res);
+            }
+            else if (sign == "/")
+            {
+                res = Convert.ToInt32(num1) / Convert.ToInt32(num2);
+                Console.WriteLine("your answer is "+res);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+//c# rating 10👍/10
